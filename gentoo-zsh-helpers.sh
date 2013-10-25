694d8bac534a20258576ff2ba12916bd0cf392d9
nixonhead <deadbilberrypie@gmail.com>
2013-10-26 00:43:29 +0700

    Add fastemerge, unmerge system-update functions

--- gentoo-zsh-helpers.sh
+++ gentoo-zsh-helpers.sh
@@ -11,7 +11,50 @@ edit-portage() {
 	else
 		echo  $keywords;
 	fi
+	cmd_str="";
+}
+
+compctl -k portage_keywords edit-portage;
+
+
+fastmerge() {
+	if [[ -z "$1" ]]; then
+		echo "I need a package name to emerge";
+	else
+		if [[ "`whoami`" == "root" ]] ; then
+			cmd_str="emerge -av $1;rehash";
+		else
+			cmd_str="sudo emerge -av $1;rehash";
+		fi
+		eval $cmd_str
+	fi
+	cmd_str="";
+}
+
+unmerge() {
+	if [[ -z "$1" ]]; then
+		echo "I need a package name to unmerge";
+	else
+		if [[ "`whoami`" == "root" ]] ; then
+			cmd_str="emerge -aC $1;rehash";
+		else
+			cmd_str="sudo emerge -aC $1;rehash";
+		fi
+		eval $cmd_str
+	fi
+	cmd_str="";	
+}
+
+system-update() {
+	if [[ "`whoami`" == "root" ]] ; then
+		cmd_str="emerge -DuvaN @world; rehash";
+	else
+		cmd_str="sudo emerge -DuvaN @world;rehash";
+	fi
+		eval $cmd_str
+	cmd_str="";	
 }
 
 
-
+#Aliases
+alias kern-update="zcat /proc/config.gz > .config && genkernel  --menuconfig --no-clean all && emerge @module-rebuild" #by  Fei Wong Reed
